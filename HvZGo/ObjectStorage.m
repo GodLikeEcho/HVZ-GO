617bdd44b9e72cb5ee663a09f65d7274f0b30b69
Jacob Shanklin <trall541@gmail.com>
2016-04-30 21:24:58 -0700

    Initial commit from local git

---
+++ HvZGo/ObjectStorage.m
@@ -0,0 +1,12 @@
+//
+//  ObjectStorage.m
+//  HvZGo
+//
+//  Created by Jacob shanklin on 4/10/16.
+//  Copyright © 2016 Jacob shanklin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface ObjectStorage
++(NSString*)Faction;
+@end
